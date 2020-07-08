be879854ef6dcf4c89423f8e9f0e69f2b7a30459
aditi <aditi.abhang91@gmail.com>
2020-07-08 11:32:34 -0500

    adding new changes

--- pocket_blog.py
+++ pocket_blog.py
@@ -2,16 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Aditi Abhang',
+        'title': 'My first pocket thought',
+        'content': "This is my first pocket blog content.",
+        'date_posted': 'July 8, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'My second pocket thought',
+        'content': "This is my second pocket blog content.",
+        'date_posted': 'July 7, 2020'
+    }
+
+]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
