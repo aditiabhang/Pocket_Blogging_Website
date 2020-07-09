a3b69451560b8a10de4868be81d6fdc0b925791b
aditi <aditi.abhang91@gmail.com>
2020-07-09 12:10:08 -0500

    adding database and relationships

--- pocket_blog.py
+++ pocket_blog.py
@@ -1,8 +1,35 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '4f1d3c4fc5e98ee5c74d14e5402c95c5'
+app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    # __repr__ & __scr__, to show how our object is printed when we print it out
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}' )"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.title}', '{self.date_posted}')"
 
 posts = [
     {
@@ -41,9 +68,15 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    form = LoginForm
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@pocket.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check your email and password.', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
