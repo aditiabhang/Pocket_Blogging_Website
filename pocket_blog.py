fd879369b199a71111d099b252122f924979761d
aditi <aditi.abhang91@gmail.com>
2020-07-09 09:26:37 -0500

    registration validation and redirecting to home page

--- pocket_blog.py
+++ pocket_blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -32,9 +32,12 @@ def about():
     return render_template('about.html', title='About')
 
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 
