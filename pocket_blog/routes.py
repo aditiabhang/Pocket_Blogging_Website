979c869239b97bff20788520976e032d2ee78933
aditi <aditi.abhang91@gmail.com>
2020-07-14 12:05:30 -0500

    added functionality for deleting the posts

--- pocket_blog/routes.py
+++ pocket_blog/routes.py
@@ -159,3 +159,7 @@ def delete_post(post_id):
     post = Post.query.get_or_404(post_id)
     if post.author != current_user:
         abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
