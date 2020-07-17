a4808ced2cb2b46890f158610fb4f34d7ab0d467
aditi <aditi.abhang91@gmail.com>
2020-07-17 15:16:32 -0500

    added code lines in every file

--- pocket_blog/users/utils.py
+++ pocket_blog/users/utils.py
@@ -25,7 +25,7 @@ def send_reset_email(user):
                   sender='noreply@demo.com',
                   recipients=[user.email])
     msg.body = f'''To reset your password, please visit the following link:
-{url_for('reset_token', token=token, _external=True)}
+{url_for('users.reset_token', token=token, _external=True)}
 
 If you did not make this request then simply ignore this email and no changes will be made.
     '''
