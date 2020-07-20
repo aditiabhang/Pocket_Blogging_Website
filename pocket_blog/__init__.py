593177e2a1a38a3c38c11ee79114e6fe1d3696d8
aditi <aditi.abhang91@gmail.com>
2020-07-20 09:20:56 -0500

    updating init file

--- pocket_blog/__init__.py
+++ pocket_blog/__init__.py
@@ -5,8 +5,6 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from pocket_blog.config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
@@ -18,6 +16,10 @@ from pocket_blog.users.routes import users
 from pocket_blog.posts.routes import posts
 from pocket_blog.main.routes import main
 
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
