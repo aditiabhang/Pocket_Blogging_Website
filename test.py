c85bfe15fa7f46ec96271965e9e90f50f16cd0a7
aditi <aditi.abhang91@gmail.com>
2020-08-03 15:15:30 -0500

    working on test cases

--- test.py
+++ test.py
@@ -3,15 +3,19 @@ from run import app
 
 
 class MyTestCase(unittest.TestCase):
+    # ensuring the flask app is set up
     def test_one(self):
         tester = app.test_client(self)
         response = tester.get('/home', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
+    # ensuring login page loads correctly
     def test_login_page(self):
         tester = app.test_client(self)
         response = tester.get('/login', content_type='html/text')
         self.assertTrue(b'Log In' in response.data)
+
+    # ensuring that the login page behaves correctly given the correct credentials
 
 
 if __name__ == '__main__':
