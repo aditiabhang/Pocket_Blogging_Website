e65b0c785c5dfd62548e4b18ece555dacab379ee
aditi <aditi.abhang91@gmail.com>
2020-08-03 15:52:20 -0500

    working on test cases

--- test.py
+++ test.py
@@ -16,6 +16,19 @@ class MyTestCase(unittest.TestCase):
         self.assertTrue(b'Log In' in response.data)
 
     # ensuring that the login page behaves correctly given the correct credentials
+    def test_login_correct(self):
+        tester = app.test_client(self)
+        response = tester.post('/login', data=dict(email='tomthecat@gmail.com', password='password'), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+        # Oops! login unsuccessful.Please check your email and password.
+
+    # ensuring that the login page behaves correctly given the incorrect credentials
+    def test_login_incorrect(self):
+        tester = app.test_client(self)
+        response = tester.post('/login', data=dict(email='tomthecat@gmail.com', password='password'),
+                               follow_redirects=True)
+        self.assertIn(b"Oops!", response.data)
 
 
 if __name__ == '__main__':
