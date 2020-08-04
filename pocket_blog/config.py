0b77ec9b560c508bb959f1f0f9f1a58bfb31f851
aditi <aditi.abhang91@gmail.com>
2020-08-04 13:08:39 -0500

    added psyco2-binary

--- pocket_blog/config.py
+++ pocket_blog/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
